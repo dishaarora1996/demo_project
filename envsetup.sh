9f34d42f669113d50599fae28546c839ea299afb
Disha Arora <disha.arora@shyamsteel.net>
2024-07-02 18:32:01 +0530

    updates

---
+++ envsetup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ -d "env" ]; 
+then
+    echo "Python virtual environment exists." 
+else
+    pip3 install virtualenv
+    virtualenv env
+    if [ $? -eq 0 ];
+    then
+        echo "Python virtual environment created successfully"
+    else 
+        echo "Failed to create Python virtual environment"
+        exit 1
+    fi
+fi
+
+source env/bin/activate
+
+
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
